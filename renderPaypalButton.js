4d9dbd200208f8c40a920585eb70c18ff798154e
noagero <164873003+noagero@users.noreply.github.com>
2024-06-27 19:37:22 +0200

    Update renderPaypalButton.js

--- renderPaypalButton.js
+++ renderPaypalButton.js
@@ -7,7 +7,7 @@ function paypalCheckoutButton(){
           <input type="hidden" name="business" value="info@candelina.hr">
           <input type="hidden" name="item_name" value="Item Name">
           <input type="hidden" name="currency_code" value="EUR">
-          <input type="hidden" name="amount" value="0.01">
+          <input type="hidden" name="amount" value="${localStorage.TOTAL}">
           <input type="image" src="http://www.paypal.com/en_US/i/btn/x-click-but01.gif" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
         </form>`;
     }
